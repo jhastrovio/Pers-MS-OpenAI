58f3b0cb28b54d77c2a749fc9e0573819ea01795
James Hassett <james.hassett@trovio.io>
2025-05-02 17:04:49 +0800

    fix: update startup.sh to use port 8000 for Azure compatibility

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start Gunicorn
-gunicorn src.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 
+gunicorn src.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 
