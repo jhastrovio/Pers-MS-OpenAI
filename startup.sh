13a7061d783b47cba25d6591629fabb1201c80a9
jhastrovio <james.hassett@trovio.io>
2025-05-04 17:10:48 +0800

    start up

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 # Start Gunicorn
-gunicorn src.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 
+gunicorn -w 1 -k uvicorn.workers.UvicornWorker app:app 
